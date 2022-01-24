390d0895de922be567bc7157e8c0f74156cf3254
Lavliet90 <60854775+Lavliet90@users.noreply.github.com>
2022-01-24 16:56:11 +0300

    run

---
+++ filmyweb/forms.py
@@ -0,0 +1,22 @@
+from django.forms import  ModelForm
+from .models import Film, DodatkoweInfo, Ocena, Aktor
+
+class FilmForm(ModelForm):
+    class Meta:
+        model = Film
+        fields = ['tytul', 'opis', 'rok', 'premiera', 'imdb_rating', 'plakat']
+
+class DodatkoweInfoForm(ModelForm):
+    class Meta:
+        model = DodatkoweInfo
+        fields = ['czas_trwania', 'gatunek']
+
+class OcenaForm(ModelForm):
+    class Meta:
+        model = Ocena
+        fields = ['gwiazdki', 'recenzja']
+
+# class AktorForm(ModelForm):
+#     class Meta:
+#         model = Aktor
+#         fields = ['imie', 'nazwisko']
